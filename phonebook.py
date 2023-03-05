fb0fa5208752e82155ed83c2e483c1b75509db6b
Mary Rose Garcia <rose.pajanel@yahoo.com>
2023-03-05 16:09:38 +1300

    Phonebook look up

--- phonebook.py
+++ phonebook.py
@@ -1,7 +1,13 @@
 def phonebook_lookup(filename):
     phonebook_dict = dict()
     unwanted_chars = ".,- "
-      with open(filename,'r') as f:
+    with open(filename,'r') as f:
             for line in f:
+
 raw_words = line.split()   phonebook_dict[raw_words[0].strip(unwanted_chars)] = raw_words[1].strip(unwanted_chars)
-      return(phonebook_dict)
+      return new_func(phonebook_dict)
+
+phonebook=phonebook_lookup("phonebook.txt")
+
+print("\nPrinting your phonebook dict as key-value pair of name-phone")
+print(phonebook_lookup("phonebook.txt"))
